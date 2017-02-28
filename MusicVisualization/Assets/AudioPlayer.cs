3bef8984322df10ab10f5597a93960b9f7d3fe39
Ashwin Nathan <ashwinnathan09@gmail.com>
2017-02-28 09:36:34 -0800

    Music Visualization P1

---
+++ MusicVisualization/Assets/AudioPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (AudioSource))]
+
+public class AudioPlayer : MonoBehaviour {
+
+	AudioSource audioSource;
+	public static float[] samples = new float[512];
+	int numSamples = 0;
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+	}
+	void Update () {
+		GetSpectrumAudioSource ();
+	}
+
+	void GetSpectrumAudioSource()
+	{
+		audioSource.GetSpectrumData(samples, numSamples, FFTWindow.Blackman);
+}
+}
---
+++ MusicVisualization/Assets/CreateCubes.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateCubes : MonoBehaviour {
+
+	public GameObject cube;
+	private GameObject[] sampleCubes = new GameObject[512];
+	public float maxScale;
+	void Start () {
+		for (int i = 0; i < 512; i++)
+		{
+			GameObject instanceCube = (GameObject)Instantiate (cube);
+			instanceCube.transform.position = this.transform.position;
+			instanceCube.transform.parent = this.transform;
+			instanceCube.name = "SampleCube" + i;
+			this.transform.eulerAngles = new Vector3 (0, -.703125f * i, 0);
+			instanceCube.transform.position = Vector3.forward * 100;
+			sampleCubes [i] = instanceCube;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < 512; i++) {
+			if (sampleCubes != null) {
+				sampleCubes [i].transform.localScale = new Vector3 (10, (AudioPlayer.samples [i] * maxScale) + 2, 10);
+			}
+		}
+	}
+}
